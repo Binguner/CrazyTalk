198ac3633f9d3d548e6d496612d46a1fe4d90a0b
Binguner <zhengzuoyu0414@gmail.com>
2018-04-24 21:58:02 +0800

    add card detial

--- app/src/main/java/com/binguner/crazytalk/CustomView/DetailActivity.java
+++ app/src/main/java/com/binguner/crazytalk/CustomView/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.binguner.crazytalk.CustomView;
 
+import android.app.AlertDialog;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,12 +10,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.binguner.crazytalk.R;
 import com.nostra13.universalimageloader.core.ImageLoader;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by xmuSistone on 2016/9/19.
@@ -24,10 +32,10 @@ public class DetailActivity extends FragmentActivity {
     public static final String EXTRA_IMAGE_URL = "detailImageUrl";
 
     public static final String IMAGE_TRANSITION_NAME = "transitionImage";
-    public static final String ADDRESS1_TRANSITION_NAME = "address1";
-    public static final String ADDRESS2_TRANSITION_NAME = "address2";
+    public static final String ADDRESS1_TRANSITION_NAME = "申请加入";
+    public static final String ADDRESS2_TRANSITION_NAME = "举报";
     public static final String ADDRESS3_TRANSITION_NAME = "address3";
-    public static final String ADDRESS4_TRANSITION_NAME = "address4";
+    public static final String ADDRESS4_TRANSITION_NAME = "丐帮";
     public static final String ADDRESS5_TRANSITION_NAME = "address5";
     public static final String RATINGBAR_TRANSITION_NAME = "ratingBar";
 
@@ -44,11 +52,16 @@ public class DetailActivity extends FragmentActivity {
     private static final String[] headStrs = {HEAD1_TRANSITION_NAME, HEAD2_TRANSITION_NAME, HEAD3_TRANSITION_NAME, HEAD4_TRANSITION_NAME};
     private static final int[] imageIds = {R.mipmap.head1, R.mipmap.head2, R.mipmap.head3, R.mipmap.head4};
 
+    /**
+     *  address1：申请加入
+     *  address3：举报
+     *  address4:  名称
+     */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
+        ButterKnife.bind(this);
         imageView = (ImageView) findViewById(R.id.image);
         address1 = findViewById(R.id.address1);
         address2 = findViewById(R.id.address2);
@@ -79,16 +92,58 @@ public class DetailActivity extends FragmentActivity {
         dealListView();
     }
 
+    @OnClick(R.id.address1)
+    public void askJoin(View view){
+        Toast.makeText(this,"Ask for Join!",Toast.LENGTH_SHORT).show();
+        View view1 = LayoutInflater.from(this).inflate(R.layout.pop_ask_join_circle,null,false);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        Spinner pop_join_i_am_spinner = view1.findViewById(R.id.pop_join_i_am_spinner);
+        EditText pop_join_ed = view1.findViewById(R.id.pop_join_ed);
+        Button pop_join_cancle_join = view1.findViewById(R.id.pop_join_cancle_join);
+        Button pop_join_ok = view1.findViewById(R.id.pop_join_ok);
+
+
+        builder.setView(view1);
+        final AlertDialog dialog = builder.show();
+
+        pop_join_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DetailActivity.this,"加入请求已发送",Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        pop_join_cancle_join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+    }
+    @OnClick(R.id.address3)
+    public void warning(View view){
+        Toast.makeText(this,"Waring this circle",Toast.LENGTH_SHORT).show();
+    }
+
     private void dealListView() {
         LayoutInflater layoutInflater = LayoutInflater.from(this);
 
         for (int i = 0; i < 20; i++) {
-            View childView = layoutInflater.inflate(R.layout.detail_list_item, null);
+            //View childView = layoutInflater.inflate(R.layout.detail_list_item, null);
+            View childView = layoutInflater.inflate(R.layout.friend_activity_card_layout, null);
+            childView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(DetailActivity.this,"请先加入圈子",Toast.LENGTH_SHORT).show();
+                }
+            });
             listContainer.addView(childView);
             ImageView headView = (ImageView) childView.findViewById(R.id.head);
             if (i < headStrs.length) {
-                headView.setImageResource(imageIds[i % imageIds.length]);
-                ViewCompat.setTransitionName(headView, headStrs[i]);
+                //headView.setImageResource(imageIds[i % imageIds.length]);
+                //ViewCompat.setTransitionName(headView, headStrs[i]);
             }
         }
     }
